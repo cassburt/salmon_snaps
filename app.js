da0c0fd837c918234811d8177904d9647ae65aee
cassburt <cass.burt@gmail.com>
2016-03-24 21:29:13 -0700

    random number generator works - now figure out why

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var salStore = {
-  location: 'Pioneer Square'
-  hours: 8
-  minCustPerHour: 17
-  maxCustPerHour: 88
-  avgCookiePerSale: 5.2
-  var randomCookiesPerHour = {
-  function getRandomIntInclusive(minCustPerHour, maxCustPerHour) {
-    return Math.floor(Math.random() * (max -min +1)) + min;
-    }
+var storeOne = {
+  location: "Pioneer Square",
+  hours: 8,
+  minCus: 17,
+  maxCus: 88,
+  avSale: 5.2,
+  genCust: function() {
+    return Math.floor(Math.random() * (this.maxCus - this.minCus + 1)) + this.minCus;
   }
+};
+var custAv = storeOne.genCust();
+function test() {
+  alert("You have "+custAv+" customers");
 };
