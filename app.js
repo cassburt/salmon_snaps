bd3e779526bae6d0f2473a14d54d14591e3e3add
cassburt <cass.burt@gmail.com>
2016-03-24 18:47:40 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var salStore = {
+  location: 'Pioneer Square'
+  hours: 8
+  minCustPerHour: 17
+  maxCustPerHour: 88
+  avgCookiePerSale: 5.2
+  var randomCookiesPerHour = {
+  function getRandomIntInclusive(minCustPerHour, maxCustPerHour) {
+    return Math.floor(Math.random() * (max -min +1)) + min;
+    }
+  }
+};
