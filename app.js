1deb709fc5cfd19ca72f0730dfd27fe13f62c2ef
cassburt <cass.burt@gmail.com>
2016-03-31 20:08:46 -0700

    newest before class

--- app.js
+++ app.js
@@ -1,94 +1,45 @@
-var cartOne = {           // Object: Salmon cookie cart
-  loc: "Pioneer Square",  // Location of cart
-  hours:  8,              // # of hours the cart is open
-  minCus: 17,             // Min # of customers per hour
-  maxCus: 88,             // Max # of customers per hour
-  avSale: 5.2,            // Av # of cookies per sale
-  cPh:  0,                // Cookies per hour
-  sales: [],              // Array for cart sales
-  operation: ["10am: ", "11am: ", "12pm: ", "1pm: ", "2pm: ", "3pm: ", "4pm: ", "5pm: "],
-  cusGen: function() {    // Random # of customers based on cart max/min
+var Cart = function (location, minimumCustomers, maximumCustomers, averageSale, sales) {
+  this.loc = location;
+  this.minCus = minimumCustomers;
+  this.maxCus = maximumCustomers;
+  this.avSale = averageSale;
+  this.sales = sales;
+  this.hours =  ["10am: ", "11am: ", "12pm: ", "1pm: ", "2pm: ", "3pm: ", "4pm: ", "5pm: "];
+  this.cusGen = function() {  //Function to generate a random # of customers based on car max/min
     return Math.random() * (this.maxCus - this.minCus) + this.minCus;
-  }
+  };
 };
+var pioSqu = new Cart("Pioneer Square", 17, 88, 5.2, []);
+var porAir = new Cart("Portland Airport", 6, 24, 1.2, []);
+var wasSqu = new Cart("Washington Square", 11, 38, 1.9, []);
+var selWoo = new Cart("Sellwood", 20, 48, 3.3, []);
+var peaDis = new Cart("Pearl District", 3, 24, 2.6, []);
 
-var cartTwo = {            // Object: Salmon cookie cart
-  loc: "Portland Airport", // Location of cart
-  hours:  8,               // # of hours the cart is open
-  minCus: 6,               // Min # of customers per hour
-  maxCus: 24,              // Max # of customers per hour
-  avSale: 1.2,             // Av # of cookies per sale
-  sales: [],               // Array for cart sales
-  operation: ["10am: ", "11am: ", "12pm: ", "1pm: ", "2pm: ", "3pm: ", "4pm: ", "5pm: "],
-  cusGen: function() {     // Random # of customers based on cart max/min
-    return Math.random() * (this.maxCus - this.minCus) + this.minCus;
-  }
-};
-
-var cartThree = {            // Object: Salmon cookie cart
-  loc: "Washington Square",  // Location of cart
-  hours:   8,                // # of hours the cart is open
-  minCus: 11,               // Min # of customers per hour
-  maxCus: 38,               // Max # of customers per hour
-  avSale: 1.9,              // Av # of cookies per sale
-  sales: [],                // Array for cart sales
-  operation: ["10am: ", "11am: ", "12pm: ", "1pm: ", "2pm: ", "3pm: ", "4pm: ", "5pm: "],
-  cusGen: function() {      // Random # of customers based on cart max/min
-    return Math.random() * (this.maxCus - this.minCus) + this.minCus;
-  }
-};
-
-var cartFour = {           // Object: Salmon cookie cart
-  loc: "Sellwood",         // Location of cart
-  hours:  8,              // # of hours the cart is open
-  minCus: 20,             // Min # of customers per hour
-  maxCus: 48,             // Max # of customers per hour
-  avSale: 3.3,            // Av # of cookies per sale
-  sales: [],              // Array for cart sales
-  operation: ["10am: ", "11am: ", "12pm: ", "1pm: ", "2pm: ", "3pm: ", "4pm: ", "5pm: "],
-  cusGen: function() {    // Random # of customers based on cart max/min
-    return Math.random() * (this.maxCus - this.minCus) + this.minCus;
-  }
-};
-
-var cartFive = {           // Object: Salmon cookie cart
-  loc: "Pearl District",   // Location of cart
-  hours:   8,              // # of hours the cart is open
-  minCus:  3,              // Min # of customers per hour
-  maxCus: 24,              // Max # of customers per hour
-  avSale: 2.6,             // Av # of cookies per sale
-  sales: [],               // Array for cart sales
-  operation: ["10am: ", "11am: ", "12pm: ", "1pm: ", "2pm: ", "3pm: ", "4pm: ", "5pm: "],
-  cusGen: function() {     // Random # of customers based on cart max/min
-    return Math.random() * (this.maxCus - this.minCus) + this.minCus;
-  }
-};
-
-function coCo(cart) {  //Calcs an array of cookies per hour
-  for (var i = 0; i < cart.hours; i++) {
+function cooCou(cart) { //Cookie Counter: Counts # of cookies / hour
+  for (var i = 0; i < cart.hours.length; i++) {
     var cPh = Math.round(cart.cusGen() * cart.avSale);
     cart.sales.push (cPh);
-  }
+  };
 };
 
-function shSa(cart, listID) {  //Function: Shows sales as unordered list
+function disSal(cart, listID) {  //Display Sales: Displays sales as unordered list
   var perHour = "";
   for (var saleIndex = 0; saleIndex < cart.sales.length; saleIndex++) {
-    perHour += "<li>" + cart.operation[saleIndex] + cart.sales[saleIndex] + " cookies" + "</li>";
+    perHour += "<li>" + cart.hours[saleIndex] + cart.sales[saleIndex] + " cookies" + "</li>";
   }
   var sum = cart.sales.reduce(function(pv, cv) { return pv + cv; }, 0);
   document.getElementById(listID).innerHTML = perHour + "Total: " + sum + " cookies";
 };
 
 window.onload = function() { //note: turn these into for loops
-  coCo(cartOne);
-  coCo(cartTwo);
-  coCo(cartThree);
-  coCo(cartFour);
-  coCo(cartFive);
-  shSa(cartOne, id="cartOne");
-  shSa(cartTwo, id="cartTwo");
-  shSa(cartThree, id="cartThree");
-  shSa(cartFour, id="cartFour");
-  shSa(cartFive, id="cartFive");
+  copCou(pioSqu);
+  cooCou(porAir);
+  cooCou(wasSqu);
+  cooCou(selWoo);
+  cooCou(peaDis);
+  disSal(pioSqu, id="pioneer");
+  disSal(porAir, id="airport");
+  disSal(wasSqu, id="washington");
+  disSal(selWoo, id="sellwood");
+  disSal(peaDis, id="pearl");
 };
